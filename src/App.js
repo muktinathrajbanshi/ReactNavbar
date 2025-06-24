e9836dbb6124d16a98d48c3ca2054884735079e5
unknown <muktinathrajbanshi786@gmail.com>
2025-06-24 14:02:25 +0545

    React-router-dom, BrowserRouter,Switch, Route,Navlink

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
+import { Switch, Route, } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+        <section className="hero_section">
+        <p>Welcome to</p>
+        <h1>Muktinath Tech Home Page</h1>
+      </section>
+    </>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <Navbar />
+        <section className="hero_section">
+        <p>Welcome to</p>
+        <h1>Muktinath Tech About Page</h1>
+      </section>
+    </>
+  );
+};
+
+const Service = () => {
+  return (
+    <>
+      <Navbar />
+        <section className="hero_section">
+        <p>Welcome to</p>
+        <h1>Muktinath Tech Services Page</h1>
+      </section>
+    </>
+  );
+};
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+        <section className="hero_section">
+        <p>Welcome to</p>
+        <h1>Muktinath Tech Contact Page</h1>
+      </section>
+    </>
+  );
+};
+
+
 
 const App = () => {
   return (
     <>
-      <Navbar />
+     <Switch>
+           <Route exact path="/">
+        <Home />
+      </Route>
+            <Route path="/about">
+        <About />
+      </Route>
+            <Route path="/service">
+        <Service />
+      </Route>
+            <Route path="/contact">
+        <Contact />
+      </Route>
+     </Switch>
     </>
   )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,6 +3,8 @@ import "./navbar.css";
 import { FaFacebookSquare, FaInstagramSquare, FaYoutubeSquare } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
 
   const [showMediaIcons, setShowMediaIcons] = useState(false);
@@ -19,17 +21,22 @@ const Navbar = () => {
         </div>
         <div className={showMediaIcons ?  "menu_link mobile_menu_link" : "menu_link"}  >
             <ul>
-                <li><a href="#"></a>Home</li>
-                <li><a href="#"></a>About</li>
-                <li><a href="#"></a>Sevices</li>
-                <li><a href="#"></a>Contact</li>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/service">Sevice</NavLink></li>
+                <li><NavLink to="/contact">Contact</NavLink></li>
             </ul>
         </div>
         <div className="social_media">
             <ul className="social_media_desktop">
-                <li><a href="https://www.youtube.com/@muktinathrajbanshi219" target="_blank"></a><FaFacebookSquare className="facebook" /></li>
-                <li><a href="https://www.youtube.com/@muktinathrajbanshi219" target="_blank"></a><FaInstagramSquare className="instagram" /></li>
-                <li><a href="https://www.youtube.com/@muktinathrajbanshi219" target="_blank"></a><FaYoutubeSquare className="youtube" /></li>
+                <li><a href="#"><FaFacebookSquare className="facebook" /></a></li>
+                <li><a href="#"><FaInstagramSquare className="instagram" /></a></li>
+                <li>
+                <a href="https://www.youtube.com/@muktinathrajbanshi219" 
+                target="_blank">
+                <FaYoutubeSquare className="youtube" />
+                </a>
+                </li>
             </ul>
                 {/* humburget menu start */}
             <div className="hamburger_menu">
@@ -40,10 +47,10 @@ const Navbar = () => {
         </div>
 
       </nav>
-      <section className="hero_section">
+      {/* <section className="hero_section">
         <p>Welcome to</p>
         <h1>Muktinath Tech</h1>
-      </section>
+      </section> */}
     </>
   );
 };
